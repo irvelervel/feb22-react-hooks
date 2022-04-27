bce7f3e3cd868c635345a59a4284213e3891e961
Stefano Casasola <ste.casasola@gmail.com>
2022-04-27 10:05:21 +0200

    First break, useState example

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import UseStateComponent from './components/UseStateComponent'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <UseStateComponent />
+      </header>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/UseStateComponent.jsx
@@ -0,0 +1,58 @@
+import { useState } from 'react'
+import { Button } from 'react-bootstrap'
+
+// useState will create a STATE VARIABLE for you!
+// {
+//     movieTitle: 'Iron Man', <-- this is a state variable!
+//      reservation: null <-- this is another state variable!
+// name: 'Stefano'
+// }
+
+// LET'S REMEMBER THE 2 RULES OF HOOKS:
+// 1) USE REACT HOOKS JUST IN REACT FUNCTIONAL COMPONENTS (no class components, no other JS)
+// 2) USE REACT HOOKS ALWAYS AT THE TOP LEVEL OF YOUR COMPONENT, BEFORE THE RETURN STATEMENT
+// AND OUTSIDE OF ANY CONDITION, FUNCTION, LOOP
+
+const UseStateComponent = () => {
+  // right here!
+  // useState return an array with 2 elements:
+  // [0] --> the state variable
+  // [1] --> it's a FUNCTION capable of changing the value of the state variable
+  // it comes with (smth like a setState, but which JUST works on the associated state variable)
+  const [name, setName] = useState('Stefano')
+  const [reservation, setReservation] = useState({
+    name: '',
+    phone: '',
+    specialRequests: '',
+  })
+  // name is going to be the state variable React is going to keep track of
+  // setName is a function capable of just assigning a new value to the name state variable (like a setState, but more specific)
+
+  // in a class component, if you wanted to change the value of name:
+  // this.setState({ name: 'Rasmus' })
+  // setName('Rasmus')
+
+  return (
+    <div>
+      <h2>useState example!</h2>
+      <p>Currently my name is: {name}</p>
+      <p>Current reservation phone: {reservation.phone}</p>
+      <Button variant="warning" onClick={() => setName('Rasmus')}>
+        CHANGE NAME
+      </Button>
+      <Button
+        variant="dark"
+        onClick={() =>
+          setReservation({
+            ...reservation,
+            phone: '000',
+          })
+        }
+      >
+        CHANGE RESERVATION PHONE
+      </Button>
+    </div>
+  )
+}
+
+export default UseStateComponent
